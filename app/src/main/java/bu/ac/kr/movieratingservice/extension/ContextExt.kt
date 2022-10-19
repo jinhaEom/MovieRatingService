cdd8710f2c215f2e3206927fb5b8716d1b789ef4
jinahaEom <umjin98@naver.com>
2022-10-19 19:13:26 +0900

    앱 UI 색상 및 extension 추가

---
+++ app/src/main/java/bu/ac/kr/movieratingservice/extension/ContextExt.kt
@@ -0,0 +1,8 @@
+package bu.ac.kr.movieratingservice.extension
+
+import android.content.Context
+import androidx.annotation.Px
+
+
+@Px
+fun Context.dip(dipValue:Float) = (dipValue * resources.displayMetrics.density).toInt()
---
+++ app/src/main/java/bu/ac/kr/movieratingservice/extension/ViewExt.kt
@@ -0,0 +1,15 @@
+package bu.ac.kr.movieratingservice.extension
+
+import android.view.View
+import androidx.annotation.Px
+
+@Px
+fun View.dip(dipValue: Float) = context.dip(dipValue)
+
+fun View.toVisible() {
+    visibility = View.VISIBLE
+}
+
+fun View.toGone(){
+    visibility = View.GONE
+}

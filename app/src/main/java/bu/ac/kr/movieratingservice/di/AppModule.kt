97845891879187091296eca5403ecabdeacc42e9
jinahaEom <umjin98@naver.com>
2022-11-08 15:00:59 +0900

    앱 실행시 오류 수정

--- app/src/main/java/bu/ac/kr/movieratingservice/di/AppModule.kt
+++ app/src/main/java/bu/ac/kr/movieratingservice/di/AppModule.kt
@@ -1,10 +1,7 @@
 package bu.ac.kr.movieratingservice.di
 
 import android.app.Activity
-import bu.ac.kr.movieratingservice.data.api.MovieApi
-import bu.ac.kr.movieratingservice.data.api.MovieFirestoreApi
-import bu.ac.kr.movieratingservice.data.api.ReviewApi
-import bu.ac.kr.movieratingservice.data.api.ReviewFirestoreApi
+import bu.ac.kr.movieratingservice.data.api.*
 import bu.ac.kr.movieratingservice.data.repository.*
 import bu.ac.kr.movieratingservice.domain.model.Movie
 import bu.ac.kr.movieratingservice.domain.usecase.GetAllMoviesUseCase
@@ -16,6 +13,9 @@ import bu.ac.kr.movieratingservice.preference.SharedPreferenceManager
 import bu.ac.kr.movieratingservice.presentation.home.HomeContract
 import bu.ac.kr.movieratingservice.presentation.home.HomeFragment
 import bu.ac.kr.movieratingservice.presentation.home.HomePresenter
+import bu.ac.kr.movieratingservice.presentation.mypage.MyPageContract
+import bu.ac.kr.movieratingservice.presentation.mypage.MyPageFragment
+import bu.ac.kr.movieratingservice.presentation.mypage.MyPagePresenter
 import bu.ac.kr.movieratingservice.presentation.review.MovieReviewsContract
 import bu.ac.kr.movieratingservice.presentation.review.MovieReviewsFragment
 import bu.ac.kr.movieratingservice.presentation.review.MovieReviewsPresenter
@@ -31,15 +31,16 @@ val appModule = module {
 val dataModule = module {
     single { Firebase.firestore }
 
-    single<MovieApi> {MovieFirestoreApi(get())}
-    single<ReviewApi> {ReviewFirestoreApi(get())}
+    single<MovieApi> { MovieFirestoreApi(get()) }
+    single<ReviewApi> { ReviewFirestoreApi(get()) }
+    single<UserApi> { UserFirestoreApi(get()) }
 
-    single<MovieRepository> {MovieRepositoryImpl(get(), get())}
-    single<ReviewRepository> {ReviewRepositoryImpl(get(), get())}
-    single<UserRepository> { UserRepositoryImpl(get(), get(), get())}
+    single<MovieRepository> { MovieRepositoryImpl(get(), get()) }
+    single<ReviewRepository> { ReviewRepositoryImpl(get(), get()) }
+    single<UserRepository> { UserRepositoryImpl(get(), get(), get()) }
 
     single { androidContext().getSharedPreferences("preference", Activity.MODE_PRIVATE) }
-    single<PreferenceManager> { SharedPreferenceManager(get())}
+    single<PreferenceManager> { SharedPreferenceManager(get()) }
 }
 val domainModule = module{
     factory { GetRandomFeaturedMovieUseCase(get(),get())}
@@ -53,5 +54,8 @@ val presenterModule = module {
     }
     scope<MovieReviewsFragment>{
         scoped<MovieReviewsContract.Presenter> { (movie: Movie) -> MovieReviewsPresenter(getSource(), movie, get())}
+    }
+    scope<MyPageFragment> {
+        scoped<MyPageContract.Presenter> { MyPagePresenter(getSource(), get()) }
     }
 }
--- app/src/main/java/bu/ac/kr/movieratingservice/domain/usecase/GetMyReviewedMoviesUseCase.kt
+++ app/src/main/java/bu/ac/kr/movieratingservice/domain/usecase/GetMyReviewedMoviesUseCase.kt
@@ -4,31 +4,34 @@ import bu.ac.kr.movieratingservice.data.repository.MovieRepository
 import bu.ac.kr.movieratingservice.data.repository.ReviewRepository
 import bu.ac.kr.movieratingservice.data.repository.UserRepository
 import bu.ac.kr.movieratingservice.domain.model.ReviewedMovie
-import com.google.firebase.firestore.auth.User
+import bu.ac.kr.movieratingservice.domain.model.User
 
 class GetMyReviewedMoviesUseCase(
-    private val userRepository : UserRepository,
-    private val reviewRepository : ReviewRepository,
-    private val movieRepository : MovieRepository
+    private val userRepository: UserRepository,
+    private val reviewRepository: ReviewRepository,
+    private val movieRepository: MovieRepository
 ) {
-    suspend operator fun invoke(): List<ReviewedMovie>{
-        val user =userRepository.getUser()
 
-        if(user ==null){
+    suspend operator fun invoke(): List<ReviewedMovie> {
+        val user = userRepository.getUser()
+
+        if (user == null) {
             userRepository.saveUser(User())
             return emptyList()
         }
-        val reviews = reviewRepository.getAllUserReviews(user.id!!)
-            .filter{ it.movieId.isNullOrBlank().not()}
 
-        if(reviews.isNullOrEmpty()){
+        val reviews = reviewRepository.getAllUserReviews(user.id!!)
+            .filter { it.movieId.isNullOrBlank().not() }
+
+        if (reviews.isNullOrEmpty()) {
             return emptyList()
         }
+
         return movieRepository
-            .getMovies(review.map {it.movieId!!})
+            .getMovies(reviews.map { it.movieId!! })
             .mapNotNull { movie ->
-                val relatedReview =reviews.find{ it.movieId == movie.id}
-                relatedReview?.let { ReviewedMovie(movie,it)}
+                val relatedReview = reviews.find { it.movieId == movie.id }
+                relatedReview?.let { ReviewedMovie(movie, it) }
             }
     }
 }
